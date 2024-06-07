92d376f47430617f838386dd58208b612e3078ae
kanyesteves <kany.esteves@gmail.com>
2024-06-07 06:34:46 -0300

    Ajustado calculo de horas e dias no BI.

--- src/main.py
+++ src/main.py
@@ -15,8 +15,9 @@ pdf_path = st.sidebar.file_uploader("Importe o relatório de faltas aqui!!")
 pdf = ExtractPdf(pdf_path)
 pdf.extract()
 
+# 1 dia equivale a 4 aulas considerando 1 hora por aula.
 def calc_faltas(horas, faltas):
-  quant_aula = (horas * 60) / 40
+  quant_aula = horas / 4
   quant_faltas = quant_aula * 0.25
   return quant_faltas
 
@@ -28,7 +29,7 @@ if len(pdf.dados_materias):
   for uc, dados in pdf.dados_materias.items():
     if uc == materia:
       result = calc_faltas(int(dados['Carga']), int(dados['Faltas']))
-      col1.metric("Carga horaria da matéria", f"{dados['Carga']} horas", f"{(int(dados['Carga']) * 60) / 40:.0f} aulas")
-      col2.metric("Quantidade de aulas que pode faltar", f"{result:.0f} aulas", "25%")
-      col3.metric("Total de faltas", f"{dados['Faltas']} faltas", f"{result - int(dados['Faltas'])} aulas para faltar")
+      col1.metric("Carga horaria da matéria", f"{dados['Carga']} horas", f"{int(dados['Carga']) / 4:.0f} dias de aula")
+      col2.metric("Quantidade de dias que pode faltar na materia", f"{result:.0f} dias", "25%")
+      col3.metric("Total de faltas", f"{dados['Faltas']} faltas", f"{int(dados["Faltas"]) / 4} dias")
         
